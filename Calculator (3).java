7d986f12a9058301bcb7c80bfb4784cc476c5ddf
Nicole Janousek <47906458+njanouse@users.noreply.github.com>
2019-02-22 18:33:50 -0700

    Add files via upload

---
+++ Calculator (3).java
@@ -0,0 +1,35 @@
+package cse360assign2;
+
+public class Calculator {
+	private int total;
+	StringBuffer history;
+	public Calculator() {
+		total = 0;
+		history = new StringBuffer("0");
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void add(int value) {
+		total = total + value;
+		history.append(" + " + value);
+	}
+	public void subtract(int value) {
+		total = total - value;
+		history.append(" - " + value);
+	}
+	public void multiply(int value) {
+		total = total * value;
+		history.append(" * " + value);
+	}
+	public void divide(int value) {
+		if (value == 0)
+			total = 0;
+		else
+			total = total/value;
+		history.append(" / " + value);
+	}
+	public String getHistory() {
+		return history.toString();
+	}
+}
